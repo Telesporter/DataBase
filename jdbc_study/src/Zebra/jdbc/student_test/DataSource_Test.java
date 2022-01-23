fc196b1d21f46d791393bfa01e7786780cf5f76d
Telesporter <51327163+Telesporter@users.noreply.github.com>
2022-01-23 21:06:46 +0800

    Add files via upload

--- jdbc_study/src/Zebra/jdbc/student_test/DataSource_Test.java
+++ jdbc_study/src/Zebra/jdbc/student_test/DataSource_Test.java
@@ -1,5 +1,8 @@
 package Zebra.jdbc.student_test;
 
+import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
+
+import javax.sql.DataSource;
 import java.math.BigDecimal;
 import java.sql.*;
 import java.util.ArrayList;
@@ -12,30 +15,41 @@ import java.util.List;
  * Time: 18:53
  * Description: No Description
  */
-public class Student_Test {
+public class DataSource_Test {
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
 //        printList(query("计算机系2019级1班"));
         printList(query("计算机系2019级1班' or '1'='1"));
     }
     public static List<Info> query(String name) throws ClassNotFoundException, SQLException {
         Connection connection = null;
-        Statement statement = null;
+        PreparedStatement statement = null;
         ResultSet resultSet = null;
 
         try {
-            Class.forName("com.mysql.jdbc.Driver");
-            connection = DriverManager.getConnection(
-                    "jdbc:mysql://localhost:3306/db2?user=root&password=123456&useUnicode=true&characterEncoding=UTF-8&useSSL=false");
+
+            MysqlDataSource ds = new MysqlDataSource();
+            ds.setURL("jdbc:mysql://localhost:3306/db2?user=root&password=123456&useUnicode=true&characterEncoding=UTF-8&useSSL=false");
+            connection = ds.getConnection();
+
             System.out.println(connection);
 
-            statement = connection.createStatement();
+            //statement = connection.createStatement();  //创建简单的Statement对象
             String sql = "select cla.name,stu.name,cou.name,sco.score from student stu\n" +
                     "left join classes cla on cla.id =stu.classes_id\n" +
                     "join score sco on sco.student_id = stu.id\n" +
                     "join course cou on cou.id = sco.course_id\n" +
-                    "where cla.name = '"+name +"'";
-            resultSet = statement.executeQuery(sql);
-            System.out.println(sql);
+                    "where cla.name =?" ;//指定多个占位符？，在执行sql的时候，替换值，不需要单引号了
+//                    "'"+name +"'";
+
+            //preparedStatement预编译的操作命令对象：注意使用String sql传入参数
+            //发送sql，让数据库预编译，语法分析，顺序分析，执行优化
+            statement = connection.prepareStatement(sql);
+            //替换占位符：指定占位符的位置（默认从1开始），数据类型
+            statement.setString(1,name); //这里不需要单引号了
+
+//            resultSet = statement.executeQuery(sql);
+            resultSet = statement.executeQuery();//因为前面已经使用preparedStatement预编译过了，不需要再次传入sql了
+            System.out.println(statement);
             List<Info> infoList = new ArrayList<>();
             while (resultSet.next()){
                 String className = resultSet.getString("cla.name");
